2228c4d487f2bdf55367e21cd705b5c32d57b3cd
hasanuzzaman-dev <hasanuzzaman.dev@gmail.com>
2023-02-06 17:14:41 +0600

    add graphql

--- src/main/java/com/graphql/learn/controller/BookController.java
+++ src/main/java/com/graphql/learn/controller/BookController.java
@@ -2,33 +2,48 @@ package com.graphql.learn.controller;
 
 
 import com.graphql.learn.entities.Book;
+import com.graphql.learn.entities.BookInput;
 import com.graphql.learn.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
     //Create
-    @PostMapping("/create-book")
-    public Book create(@RequestBody Book book){
-        return bookService.create(book);
+
+    @MutationMapping("createBook")
+    public Book create(@Argument BookInput book) {
+        Book b = new Book();
+
+        b.setTitle(book.getTitle());
+        b.setDesc(book.getDesc());
+        b.setAuthor(book.getAuthor());
+        b.setPrice(book.getPrice());
+        b.setPages(book.getPages());
+
+        return bookService.create(b);
     }
 
-    @GetMapping("/get-books")
-    public List<Book> getAllBook(){
+    @QueryMapping("allBooks")
+    public List<Book> getAllBook() {
         return bookService.getAllBook();
     }
 
-    @GetMapping("/{bookId}")
-    public Book getBookById(@PathVariable String bookId){
+    @QueryMapping("getBookById")
+    public Book getBookById(@Argument String bookId) {
         return bookService.getBookById(bookId);
     }
 
 }
+
